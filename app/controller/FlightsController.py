fd24d332e9a791332b66743fd7e4f73fb37c22e8
yts1234 <titoyosua@gmail.com>
2020-08-23 22:04:08 +0700

    1. Modify app/__init__.py import flights from model 2. Modify FlightsController.py, change the source code for get data from database 3. create __init__.py in app/model 4. create flights model

--- app/controller/FlightsController.py
+++ app/controller/FlightsController.py
@@ -1,5 +1,25 @@
+from app.model.flights import Flights
 from app import response, app
 
 
 def index():
-    return response.ok("Hai Dari Controller PT Aviator Nusantara!", "OK")
+    try:
+        flights = Flights.query.all()
+        data = transform(flights)
+        return response.ok(data, "OK")
+    except Exception as e:
+        print(e)
+
+def transform(flights):
+    array = []
+    for i in flights:
+        array.append({
+            'airline_code' : i.id,
+            'flightNumber' : i.flight_number,
+            'departurePort' : i.departure_port,
+            'arrivalPort' : i.arrival_port,
+            'departureTime' : i.departure_time,
+            'arrivalTime' : i.arrival_time,
+            })
+    return array
+
---
+++ app/model/flights.py
@@ -0,0 +1,17 @@
+from app import db
+from datetime import datetime
+
+class Flights(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    airline_code = db.Column(db.String(20), index=True, unique=True, \
+            nullable=False)
+    flight_number = db.Column(db.String(20), nullable=False)
+    departure_port = db.Column(db.String(5), nullable=False)
+    arrival_port = db.Column(db.String(5), nullable=False)
+    departure_time = db.Column(db.DateTime)
+    arrival_time = db.Column(db.DateTime)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Flights {}>'.format(self.name)
