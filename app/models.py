d4724e8bbe09e4664d75ce2158ba67b63fdc6923
yts1234 <titoyosua@gmail.com>
2020-08-23 20:58:06 +0700

    1. Modify typo in __init__.py 2. Create models.py for migrating database 3. Create config.py for DB connection

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from app import db
+from datetime import datetime
+
+class Flights(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    airline_code = db.Column(db.String(20), nullable=False, index=True, \
+            unique=True)
+    flight_number = db.Column(db.String(20), nullabe=False)
+    departure_port = db.Column(db.String(5), nullable=False)
+    arrival_port = db.Column(db.String(5), nullable=False)
+    departure_time = db.Column(db.DateTime) 
+    arrival_time = db.Column(db.DateTime)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Flights {}>'.format(self.name)
+
